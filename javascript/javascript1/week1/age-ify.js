2e43a176fb5ac9c13c95809b6710bb6dce974d4c
Sayyed Wasim Hussain Bukhari <wasimhussain3@gmail.com>
2025-02-03 13:12:08 +0100

    Little change in code of age-ify.js and index.html

--- javascript/javascript1/week1/age-ify.js
+++ javascript/javascript1/week1/age-ify.js
@@ -1,20 +1,5 @@
 
-
-document.getElementById("human-age").addEventListener("submit", function(event) 
-{
-    event.preventDefault();
-
-    const birthYear = document.getElementById("birth-year").value;
-    const futureYear = document.getElementById("future-year").value;
-    const humanAge = futureYear - birthYear;
-    console.log(humanAge);
-    document.getElementById("result").textContent = humanAge;
-});
-
-
-/*
 const yearOfBirth = 1984;
 const yearFuture = 2045;
 const age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in 2045.");
-*/
