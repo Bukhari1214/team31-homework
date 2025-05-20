70af3801c0a928d02e6b6dfd5045436e32af9547
Sayyed Wasim Hussain Bukhari <wasimhussain3@gmail.com>
2025-05-20 22:59:29 +0200

    Warmup Exercise and Some Exercises in an EXERCISE Folder in WEEK2

---
+++ nodejs/week2/Exercises/database.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import knex from "knex";
+
+dotenv.config();
+const knexInstance = knex({
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+export default knexInstance;
